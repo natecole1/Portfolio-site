5ca60ff37c22aae0875a7c112cfbaacb0ce378b5
Nate Cole <natercole@outlook.com>
2025-04-26 11:00:42 -0400

    build fix

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -1,13 +1,10 @@
 'use client'
 import React from 'react';
 import Image from 'next/image';
-import gsap from 'gsap';
-import SplitType from 'split-type'
-import { useGSAP } from '@gsap/react';
+
 
 const AboutSection = () => {
-  // const mySplitText = SplitType.create('.quote')
-  // console.log(mySplitText);
+ 
 
   return (
     <div id="about" className="w-full flex flex-col bg-black p-4 xl:py-20">
@@ -17,7 +14,7 @@ const AboutSection = () => {
       <div className="flex flex-col lg:flex-row">
         <div className=" w-full h-[550px] lg:w-[50%] flex justify-center items-center p-10">
           <p className="quote text-white xl:text-2xl">
-            {" "}
+            
             An innovative problem-solver with a passion for building cool
             things. I enjoy discovering new technologies to see what I can build
             with them. Over the past few years, I&apos;ve been immersed in the
