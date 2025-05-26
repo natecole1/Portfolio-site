24b0eaa17fbf3a5423b952ef6757ea624f0b56da
Nate Cole <natercole@outlook.com>
2025-05-26 10:51:33 -0400

    fix build error

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -48,16 +48,21 @@ const AboutSection = () => {
   return (
     <div
       id="about"
-      className="w-full z-50 bg-[#09090b]/[0.9] flex flex-col  p-4 xl:py-20" 
+      className="w-full z-50 bg-[#09090b]/[0.9] flex flex-col  p-4 xl:py-20"
     >
-
       <div className="w-30 md:w-40 flex items-center justify-center p-4 border-2 rounded-full text-white">
         <h1>About Me</h1>
       </div>
       <div className="flex flex-col lg:flex-row">
         <div className=" w-full h-[550px] lg:w-[50%] flex justify-center items-center p-10">
           <p id="quote" className=" text-white md:text-[30px] 2xl:text-[40px]">
-             I&apos;m a frontend engineer immersed in the JavaScript ecosystem. Over the last few years, I've leveraged Next.js optimization features to spin up fully responsive and accessible applications, including a fullstack podcast app. Check it out below. I&apos;m also passionate about discovering and learning from quality technical and educational content in the community. If you come across something you think I should know about, please feel free to share below. 
+            I&apos;m a frontend engineer immersed in the JavaScript ecosystem.
+            Over the last few years, I&apos;ve leveraged Next.js optimization
+            features to spin up fully responsive and accessible applications,
+            including a fullstack podcast app. Check it out below. I&apos;m also
+            passionate about discovering and learning from quality technical and
+            educational content in the community. If you come across something
+            you think I should know about, please feel free to share below.
           </p>
         </div>
         <div
